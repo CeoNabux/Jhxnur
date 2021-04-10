946fcb41ccbd56f23a62238fbc81e5d6831f1f5c
CeoNabux <lualreye@gmail.com>
2021-04-09 19:42:31 -0500

    comments added in header

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,9 +1,13 @@
 <template lang="pug">
+  //- SE ABRE EL HEADER
   nav.navbar.is-fluid.has-background-black.p-0.is-flex.justify-content-center
+    //-CONTENEDOR CON LOS ELEMENTOS DEL HEADER
     .container.is-widescreen.is-flex.is-justify-content-space-between.is-align-items-center
+      //-LOGO
       figure.image.is-64x64.is-flex.is-align-items-center
         img(src='@/assets/logo/logo-white.svg')
 
+      //-MENU-BURGER
       .is-flex.is-align-items-center
         p.has-text-white.m-2 MENU
         figure.is-image.is-24x24.is-flex.is-align-items-center.has-text-white
