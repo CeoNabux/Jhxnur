ae41334f17481d62fa82259ba4f74ed07fc59bb5
CeoNabux <lualreye@gmail.com>
2021-04-12 21:57:23 -0500

    portfolioSection rendered

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,16 +3,19 @@
     .container.if-fullhd
       the-hero
       the-extras
+      the-portfolio
 </template>
 
 <script>
 import TheHero from "@/components/sections/TheHero.vue";
 import TheExtras from "@/components/sections/TheExtras.vue";
+import ThePortfolio from "@/components/sections/ThePortfolio.vue";
 
 export default {
   components: {
     TheHero,
     TheExtras,
+    ThePortfolio,
   },
 };
 </script>
--- src/components/sections/ThePortfolio.vue
+++ src/components/sections/ThePortfolio.vue
@@ -1,5 +1,29 @@
 <template lang="pug">
   section.container.is-widescreen
     .container
-      .column.is-is-desktop
+      .content
+        h2 JhxnUrb
+        p Conoce mi trabajo artistico
+    .container
+      .column.is-desktop(v-for="project, i in projects" :key="i")
+        .column.is-2
+          .container
+            figure.image.is-1by1
+              img(:src="project.image")
+          .card-content
+            .card-title.has-text-centered {{ project.title }}
+
 </template>
+
+<script>
+export default {
+  data: () => ({
+    projects: [
+      {
+        image: require("@/assets/noBackgroundPhotos/chica-de-cerca.png"),
+        title: "Titulo de ejemplo",
+      },
+    ],
+  }),
+};
+</script>
