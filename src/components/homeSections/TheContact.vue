e7c82e19bbfd9679c6c1be43f219a2416fabed30
CeoNabux <lualreye@gmail.com>
2021-04-16 00:42:44 -0500

    contactSection structure declare

--- src/components/homeSections/TheContact.vue
+++ src/components/homeSections/TheContact.vue
@@ -2,8 +2,13 @@
   .container.is-widescreen
     .container.is-flex.is-flex-direction-column
       .content
+        h2.is-size-2.is-size-3-mobile ¿Tienes un proyecto en mente?
+        p.is-size-5 Cuéntame qué construiremos
       .form
       .container.is-flex
         .columns
+          .column
+            figure.image.is-4by3
+              img(:src="")
           .column
 </template>
