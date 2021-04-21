46770a65f8966bb07c5f2de02b8362e961c0776f
CeoNabux <lualreye@gmail.com>
2021-04-21 13:44:20 -0500

    font color added

--- src/components/pages/Extras.vue
+++ src/components/pages/Extras.vue
@@ -1,5 +1,18 @@
 <template lang="pug">
   .container.is-widescreen
     .content
-      h2.is-size-3.has-text-orange Crea tu propio arte Urbano
+      h2.is-size-2.font-color Crea tu propio arte Urbano
 </template>
+
+<style scoped>
+.font-color {
+  background: -webkit-linear-gradient(
+    270deg,
+    rgba(254, 237, 44, 1) 3%,
+    rgba(156, 65, 8, 1) 83%
+  );
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+  color: orange;
+}
+</style>
