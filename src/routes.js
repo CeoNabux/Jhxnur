97e524b8377c43e54ba1832ba9679cfd3da0de25
CeoNabux <lualreye@gmail.com>
2021-04-21 11:00:21 -0500

    routes added

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,23 @@
 import Me from "@/components/pages/Me.vue";
 import Home from "@/components/Home.vue";
+import Portfolio from "@/components/pages/Portfolio.vue";
+import Extras from "@/components/pages/Extras.vue";
 
 const routes = [
   {
     path: "/",
     component: Home,
     name: "home",
+  },
+  {
+    path: "/portfolio",
+    component: Portfolio,
+    name: "portfolio",
+  },
+  {
+    path: "/extras",
+    component: Extras,
+    name: "extras",
   },
   {
     path: "/me",
