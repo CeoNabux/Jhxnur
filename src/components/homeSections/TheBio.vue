30f9e67826bf75c1c85eb093cace557579a9d97d
CeoNabux <lualreye@gmail.com>
2021-04-22 03:40:14 -0500

    scroll buf fixed

--- src/components/homeSections/TheBio.vue
+++ src/components/homeSections/TheBio.vue
@@ -1,38 +1,27 @@
 <template lang="pug">
-  section.container.is-widescreen.my-6
-    .columns.is-desktop
-      .column.is-full-mobile.is-half-desktop
-        .content
+  section.container.is-widescreen.my-6.px-2
+    .columns.is-desktop.px-2.m-0
+      .column
+        .content.px-4
           p.is-size-4.has-text-white.mb-3 Hola, soy
           h2.letter-spacing.is-size-2.is-size-3-mobile.has-text-white.m-0 Jhon León
-        .content
+        .content.px-4
           p.is-size-4.has-text-white.mb-3 y me dedico a
           h2.letter-spacing.is-size-2.is-size-3-mobile.has-text-white.m-0 crar contenido digital
-        .content
+        .content.px-4
           p.is-size-4.has-text-white.mb-3 dirigido a
           h2.letter-spacing.is-size-2.is-size-3-mobile.has-text-white.m-0 empresas y marcas en redes sociales
-        router-link(to="/me")
+        router-link.px-4(to="/me")
           b-button.is-warning(alternate) Quieres conocer más sobre
       .column.auto
-        .container
-          figure.image
-            img.imageBack(scr="#")
-          figure.image
-            img.imageFront(scr="#")
+        .container.px-4
+          .contaner
+            img.imageBack(scr="@/assets/noBackgroundPhotos/chico-de-pie-escalera.png")
+          .container
+            img.imageFront(scr="@/assets/noBackgroundPhotos/foto-arrugada.png")
 
 
 </template>
-
-<script>
-export default {
-  // data: () => ({
-  //   images: {
-  //     imageBack: require("@/img/chico-de-pie-escalera.png"),
-  //     imageFront: require("@/img/foto-arrugada.png"),
-  //   },
-  // }),
-};
-</script>
 
 <style scoped>
 .letter-spacing {
