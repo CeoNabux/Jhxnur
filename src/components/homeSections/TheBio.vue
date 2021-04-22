e4ad0ccb9022b7cc0e99e82ddcfb62192fde8d12
CeoNabux <lualreye@gmail.com>
2021-04-22 05:57:20 -0500

    extras template done

--- src/components/homeSections/TheBio.vue
+++ src/components/homeSections/TheBio.vue
@@ -13,12 +13,12 @@
           h2.letter-spacing.is-size-2.is-size-3-mobile.has-text-white.m-0 empresas y marcas en redes sociales
         router-link.px-4(to="/me")
           b-button.is-warning(alternate) Quieres conocer más sobre
-      .column.auto
-        .container.px-4
-          .contaner
-            img.imageBack(scr="@/assets/noBackgroundPhotos/chico-de-pie-escalera.png")
-          .container
-            img.imageFront(scr="@/assets/noBackgroundPhotos/foto-arrugada.png")
+      .column
+        .container.px-4.is-relative
+          figure.image-container2
+            img.imageBack
+          figure.image-container
+            img.imageFront
 
 
 </template>
@@ -27,15 +27,40 @@
 .letter-spacing {
   letter-spacing: 12px;
 }
-.imageBack {
-  width: 250px;
-  height: 250px;
-}
 .imageFront {
+  width: 100%;
+  height: 100%;
+  background-image: url("../../assets/noBackgroundPhotos/foto-arrugada.png");
+  background-position: center;
+  background-repeat: no-repeat;
+  padding: 12px;
+}
+.image-container {
+  width: 220px;
+  height: 260px;
+  margin: 0 auto;
   position: absolute;
-  bottom: 0;
-  right: 0;
-  width: 50px;
-  height: 50px;
+  bottom: -10%;
+  right: 15%;
+}
+.image-container2 {
+  width: 350px;
+  height: 420px;
+  margin: 0 auto;
+}
+.imageBack {
+  width: 100%;
+  height: 100%;
+  background-image: url("../../assets/noBackgroundPhotos/chico-de-pie-escalera.png");
+  background-origin: center;
+  background-repeat: no-repeat;
+  padding: 12px;
+}
+
+@media only screen and (max-width: 425px) {
+  .image-container2 {
+    width: 274px;
+    padding: 6px;
+  }
 }
 </style>
