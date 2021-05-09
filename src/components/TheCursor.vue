a653cdba56ea02f7e5964ec9461f45407a057df1
CeoNabux <lualreye@gmail.com>
2021-05-08 23:45:56 -0500

    event click added to cursor

--- src/components/TheCursor.vue
+++ src/components/TheCursor.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  #cursor(:style="{ top: this.y, left: this.x }")
+  .cursor(:style="{ top: this.y, left: this.x }" :class="{'expand': clicky}")
 </template>
 
 <script>
@@ -8,9 +8,11 @@ export default {
   data: () => ({
     x: 0,
     y: 0,
+    clicky: false,
   }),
   mounted: function () {
     this.cursor();
+    this.cursorClick();
   },
   methods: {
     cursor() {
@@ -19,9 +21,17 @@ export default {
     cursorOut() {
       window.removeEventListener("mousemove", this.ubicacion);
     },
+    cursorClick() {
+      document.addEventListener("click", () => {
+        this.clicky = true;
+        setTimeout(() => {
+          this.click = false;
+        }, 500);
+      });
+    },
     ubicacion(event) {
-      this.x = event.pageX - 32 + "px";
-      this.y = event.pageY - 32 + "px";
+      this.x = event.pageX - 16 + "px";
+      this.y = event.pageY - 16 + "px";
     },
   },
   destroyed: function () {
@@ -31,16 +41,61 @@ export default {
 </script>
 
 <style scoped>
-#cursor {
-  width: 64px;
-  height: 64px;
+.cursor {
+  width: 32px;
+  height: 32px;
   border-radius: 50%;
-  border: 8px solid white;
+  border: 1px solid white;
   opacity: 0.6;
   position: absolute;
   z-index: 45;
   transition-timing-function: ease-out;
-  cursor: none;
   pointer-events: none;
+  animation: cursorAnim 0.5s infinite alternate;
+}
+
+.cursor::after {
+  content: "";
+  width: 48px;
+  height: 48px;
+  position: absolute;
+  border: 8px solid gray;
+  border-radius: 50%;
+  opacity: 0.5;
+  top: -9px;
+  left: -9px;
+  animation: cursorAnim2 0.5 infinite alternate;
+}
+@keyframes cursorAnim {
+  from {
+    transform: scale(1);
+  }
+  to {
+    transform: scale(0.7);
+  }
+}
+@keyframes cursorAnim2 {
+  from {
+    transform: scale(1);
+  }
+  to {
+    transform: scale(0.4);
+  }
+}
+@keyframes cursorAnim3 {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(3);
+  }
+  100% {
+    transform: scale(1);
+    opacity: 0;
+  }
+}
+.expand {
+  animation: cursoAnim3 0.5 infinite;
+  border: 1px solid #42b983;
 }
 </style>
